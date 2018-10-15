0d34daa0eaccd450b2c0b6bf91cca0e5f9f9787b
SnowyLuma <SnowyLuma@gmail.com>
2018-10-15 08:06:02 +0200

    Fix example file having wrong argument order

--- aivar.py
+++ aivar.py
@@ -1,5 +1,5 @@
 import aivar
 
-aivar.steam('https://www.youtube.com/watch?v=oFzx0a7IE9Y', '831230', '76561197983791150', icon_on_screen_size=32)
-aivar.steam('https://www.youtube.com/watch?v=83GF-RF3Xrc', '917240', '76561197983791150', icon_on_screen_size=32)
-aivar.steam('https://www.youtube.com/watch?v=hWgY7BFcLyc', '654670', '76561197983791150')
+aivar.steam('https://www.youtube.com/watch?v=oFzx0a7IE9Y', '76561197983791150', '831230', icon_on_screen_size=32)
+aivar.steam('https://www.youtube.com/watch?v=83GF-RF3Xrc', '76561197983791150', '917240', icon_on_screen_size=32)
+aivar.steam('https://www.youtube.com/watch?v=hWgY7BFcLyc', '76561197983791150', '654670')
