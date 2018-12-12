711ea4b3930fa6b14dddfefc981636865f25612f
luchaos <luchaos@gmail.com>
2018-12-12 12:46:42 +0100

    moved repository

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='aivar',
       version='0.1',
       description='Aivar - Achievements In Video AnnotatoR',
-      url='https://github.com/100Pals/aivar',
+      url='https://github.com/completionist/aivar',
       author='luchaos,add-yourself',
       author_email='lcs@luchaos.at,add-yourself@example.com',
       license='MIT',
